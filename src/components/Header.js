9dc883f5b098eb97c411591c3dda483ec3fa2187
manuela medina <manu573@live.com>
2023-04-16 20:03:59 -0400

    updates

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from "react"
+function Header(){
+    return(
+        <header>
+        <nav class="text-right">
+          <ul>
+            <li><a href="https://github.com/">Github</a></li>
+            <li><a href="#">Portfolio</a></li>
+            <li><a href="#contact">Contact</a></li>
+          </ul>
+        </nav>
+      </header>
+    )
+}
+export default Header
